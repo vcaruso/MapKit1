6aad62f66eedef10c8e4cb3a36f678440b9f4f34
Vincenzo Caruso <enzo@carusovincenzo.com>
2022-09-28 13:56:35 +0200

    Initial Commit

---
+++ MapKit1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapKit1
+//
+//  Created by Vincenzo Caruso on 28/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapKit1/MapKit1App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit1App.swift
+//  MapKit1
+//
+//  Created by Vincenzo Caruso on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MapKit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
